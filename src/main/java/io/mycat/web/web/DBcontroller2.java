60bb5110f3ff9a5f1bc216225084126ac541c686
长宏 <3200601@qq.com>
2016-11-25 16:28:18 +0800

    web 配置

---
+++ src/main/java/io/mycat/web/web/DBcontroller2.java
@@ -0,0 +1,14 @@
+package io.mycat.web.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by 长宏 on 2016/11/25 0025.
+ */
+@Controller
+public class DBcontroller2 {
+//    @GetMapping(value = "/")
+    public String get() {
+        return "index";
+    }
+}
