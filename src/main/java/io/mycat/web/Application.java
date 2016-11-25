80d0a995b8164cf3697c91c9075139945023bfab
长宏 <3200601@qq.com>
2016-11-25 14:16:18 +0800

    init

--- src/main/java/io/mycat/web/Application.java
+++ src/main/java/io/mycat/web/Application.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by 长宏 on 2016/11/25 0025.
+ * 启动类
  */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        MycatStartup.startmain(args);
-        SpringApplication.run(Application.class, args);
+        MycatStartup.startmain(args);//启动mycat服务器
+        SpringApplication.run(Application.class, args);//启动tomcat，web服务器
     }
 }
