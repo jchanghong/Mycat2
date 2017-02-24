657757e6d41d9fab01d2c56786bb40d1cc3fa81f
jiangchanghong <3200601@qq.com>
2017-02-24 19:13:46 +0800

    testorientdb

--- src/main/java/io/mycat/TestOrientDB.java
+++ src/main/java/io/mycat/TestOrientDB.java
@@ -1,7 +1,19 @@
 package io.mycat;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
 /**
  * Created by 长宏 on 2017/2/23 0023.
  */
 public class TestOrientDB {
+    public static void main(String[] args) {
+        ODatabaseDocumentTx db = new ODatabaseDocumentTx
+                ("plocal:database/changhong")
+                .open("admin", "admin");
+        ODocument animal = new ODocument("t1");
+        animal.field("id", "66");
+        animal.save();
+    }
+
 }
