fb67b4a9b5c66c294ed1e8527e3b42cf7f8cca39
jiangchanghong <3200601@qq.com>
2017-02-25 16:02:41 +0800

    testorientdb

--- src/main/java/io/mycat/databaseorient/TestOrientDB.java
+++ src/main/java/io/mycat/databaseorient/TestOrientDB.java
@@ -3,15 +3,23 @@ package io.mycat.databaseorient;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import io.mycat.databaseorient.adapter.DBadapter;
+import io.mycat.databaseorient.adapter.OrientException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 长宏 on 2017/2/23 0023.
  * 用来演示orientdb
  */
 public class TestOrientDB {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OrientException {
+        DBadapter.currentDB = "changhong";
+        List<Map<String, String>> list = DBadapter.getInstance().exequery("select * from t1");
+        System.out.println(list.size());
+
         ODatabaseDocumentTx db = new ODatabaseDocumentTx
                 ("plocal:database/changhong")
                 .open("admin", "admin");
